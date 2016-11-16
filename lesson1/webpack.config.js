ba2b06cff48b310b5cd79ec27afa1686288a445d
sasha.bashliy <sashkacrab@gmail.com>
2016-11-16 20:34:16 +0200

    first success with webpack

---
+++ lesson1/webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  entry: './home',  // which module to pack
+  output: {
+    filename: 'build.js'
+  }
+}
---
+++ lesson1/welcome.js
@@ -0,0 +1,5 @@
+function welcome(m){
+    alert(`Welcome ${m}`)
+}
+
+module.exports = welcome
