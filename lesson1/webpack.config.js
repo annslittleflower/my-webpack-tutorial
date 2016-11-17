da03cf86b560bc01c0b03e349752204c9d407d9a
sasha.bashliy <sashkacrab@gmail.com>
2016-11-17 14:20:48 +0200

    now babel is present

--- lesson1/webpack.config.js
+++ lesson1/webpack.config.js
@@ -1,8 +1,22 @@
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
 module.exports = {
   entry: './home',  // which module to pack
   output: {
     filename: 'build.js',
     library: 'home'
   },
-  watch:true,
+  watch: NODE_ENV == 'development',
+  devtool: NODE_ENV == 'development' ? "source-map": null,
+
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
+    }]
+  }
 }
